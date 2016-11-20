d7cf0ebca3942ccf2354c103fa07a11456a467f6
CarolinaSantana <carolina.ingsm@gmail.com>
2016-11-20 15:57:27 +0000

    Change to postgres db and ready to launch in a Docker container

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'test', email: 'test@test.com', password: 'test12', password_confirmation: 'test12')
