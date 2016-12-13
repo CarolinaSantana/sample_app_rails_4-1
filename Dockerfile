d851e69a52dab61e985117fcd52f944a356b0724
CarolinaSantana <carolina.ingsm@gmail.com>
2016-12-13 19:29:36 +0000

    Image name changed

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@
 # Version 0.1                                    #
 ##################################################
 FROM ruby:2.0-onbuild
-LABEL sample_app_image_0_1.version="0.1" sample_app_image_0_1.release-date="2016-12-10"
+LABEL sample_app_rails_4_image.version="0.1" sample_app_rails_4_image.release-date="2016-12-10"
 MAINTAINER Carolina Santana "c.santanamartel@gmail.com"
 RUN apt-get update && apt-get -y install nodejs && \
     apt-get autoclean && apt-get clean && \
--- monomachine-docker.sh
+++ monomachine-docker.sh
@@ -5,7 +5,7 @@ if [ "$POSTGRES_USER" = "" ] || [ "$POSTGRES_PASSWORD" = "" ]; then
         exit
 fi
 
-docker build -t sample_app_image_0_1 .
+docker build -t sample_app_rails_4_image .
 
 docker run --name some-postgres -e POSTGRES_USER=$POSTGRES_USER \
            -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres
@@ -15,7 +15,7 @@ docker volume create --name volume-public
 docker run -d -it --name some-app -e POSTGRES_USER=$POSTGRES_USER \
            -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -w /usr/src/app \
            -v volume-public:/usr/src/app/public \
-           --link some-postgres:db sample_app_image_0_1 tail -f /dev/null
+           --link some-postgres:db sample_app_rails_4_image tail -f /dev/null
 
 docker exec -d some-app rake db:create
 docker exec -d some-app rake db:migrate
