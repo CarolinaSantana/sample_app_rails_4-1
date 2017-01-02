c4c89950c42f3defac44987ecad82f43c5e73125
CarolinaSantana <carolina.ingsm@gmail.com>
2017-01-02 15:27:52 +0000

    Some corrections in the order of the deployment script

--- monomachine-docker.sh
+++ monomachine-docker.sh
@@ -8,23 +8,16 @@ fi
 docker build -t sample_app_rails_4_image .
 
 docker run --name some-postgres -e POSTGRES_USER=$POSTGRES_USER \
-           -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres
 
 docker volume create --name volume-public
 
 docker run -d -it --name some-app -e POSTGRES_USER=$POSTGRES_USER \
-           -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -w /usr/src/app \
-           -v volume-public:/usr/src/app/public \
-           --link some-postgres:db sample_app_rails_4_image tail -f /dev/null
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -w /usr/src/app \
+  -v volume-public:/usr/src/app/public --link some-postgres:db sample_app_rails_4_image
 
-docker exec -d some-app rake db:create
-docker exec -d some-app rake db:migrate
-docker exec -d some-app rake db:seed
-docker exec -d some-app rake db:populate
+docker run --name some-nginx -v "${PWD}/nginx.conf":/etc/nginx/conf.d/default.conf \
+  -p 8080:80 --link some-app:app -v volume-public:/usr/src/app/public -d nginx
 
-docker run --name some-nginx \
-           -v "${PWD}/nginx.conf":/etc/nginx/conf.d/default.conf \
-           -p 80:80 --link some-app:app -v volume-public:/usr/src/app/public -d nginx
-
-
+docker exec some-app bash -c 'rake db:create ; rake db:migrate ; rake db:seed ; rake db:populate'
 
