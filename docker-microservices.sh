660263719ea6957babf2271d0bfd3395643daa90
CarolinaSantana <carolina.ingsm@gmail.com>
2017-02-13 15:06:01 +0000

    Testing application_controller.rb

--- docker-microservices.sh
+++ docker-microservices.sh
@@ -17,10 +17,10 @@ docker run -i --name app-job --entrypoint /setup.sh -e POSTGRES_USER=$POSTGRES_U
   -v volume-public:/usr/src/app/public --link some-postgres:db sample_app_rails_4_image
 
 docker run -d -it --name app-task -e POSTGRES_USER=$POSTGRES_USER \
-  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -w /usr/src/app \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -w /usr/src/app -p 80:80 \
   -v volume-public:/usr/src/app/public --link some-postgres:db sample_app_rails_4_image \
   /bin/bash -c "cp config/database.yml.postgresql config/database.yml && puma -p 9292"
 
 docker run --name some-nginx -v "${PWD}/nginx.conf":/etc/nginx/conf.d/default.conf \
-  -p 8080:80 --link app-task:app -v volume-public:/usr/src/app/public -d nginx
+  -p 8080:8080 --link app-task:app -v volume-public:/usr/src/app/public -d nginx
 
